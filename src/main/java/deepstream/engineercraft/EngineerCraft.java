6a91456021e47d8aba25779a5605af5b5125c784
Deepstream <william.deepstream@gmail.com>
2014-06-23 20:13:01 +0200

    Adding the mcmod.info and mod class

---
+++ src/main/java/deepstream/engineercraft/EngineerCraft.java
@@ -0,0 +1,30 @@
+package deepstream.engineercraft;
+
+import cpw.mods.fml.common.Mod;
+import cpw.mods.fml.common.event.FMLPreInitializationEvent;
+
+@Mod(modid = EngineerCraft.MODID, name = EngineerCraft.NAME, version = EngineerCraft.VERSION)
+public class EngineerCraft {
+	public static final String MODID = "EngineerCraft";
+	public static final String NAME = "EngineerCraft";
+	public static final String VERSION = "1.7.2-0.1";
+
+	@Mod.Instance
+	public static EngineerCraft instance;
+
+	@Mod.EventHandler
+	public static void preInit(FMLPreInitializationEvent e) {
+
+	}
+
+	@Mod.EventHandler
+	public static void init(FMLPreInitializationEvent e) {
+
+	}
+
+	@Mod.EventHandler
+	public static void postInit(FMLPreInitializationEvent e) {
+
+	}
+
+}
